3ff27aecdc234c43eabfa5e0804f851e2f525739
taku26841397 <taku26841397@gmail.com>
2026-04-25 16:06:58 +0900

    add
    add

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from crud import views
 from django.conf import settings
 from django.conf.urls.static import static
+from crud.views import create_test_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
