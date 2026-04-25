0797940a7c287f168ac18b4039326b03e37f530c
taku26841397 <taku26841397@gmail.com>
2026-04-25 12:56:15 +0900

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--&(=p=fm-b1_+q309z3pcb=3=rlz*3od($i+$w*ai+&(ar=ht5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://portfolio-crud-j2ow.onrender.com']
+ALLOWED_HOSTS = ['portfolio-crud-j2ow.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://portfolio-crud-j2ow.onrender.com/'
