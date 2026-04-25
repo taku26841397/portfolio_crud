4dbc0e216dedec6c8d1ece84c2c0db374e906fb8
taku26841397 <taku26841397@gmail.com>
2026-04-25 13:04:09 +0900

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.TopView.as_view(),name="top"),
+    path('',views.LoginView.as_view(),name="login"),
     path('crud/',views.ProductListView.as_view(),name="list"),
     path('crud/new/',views.ProductCreateView.as_view(),name="new"),
     path('crud/edit/<int:pk>',views.ProductUpdateView.as_view(),name="edit"),
