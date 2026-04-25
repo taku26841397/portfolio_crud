efbfc33ba1b579ab57229339436e7e9fd7d87c75
taku26841397 <taku26841397@gmail.com>
2026-04-25 12:41:49 +0900

    Add files via upload

---
+++ crud/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView,DetailView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from .models import Product
+from django.urls import reverse_lazy
+from django.urls import reverse
+from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class TopView(TemplateView):
+    template_name = "top.html"
+
+class ProductListView(LoginRequiredMixin,ListView):
+    model=Product
+    paginate_by=3
+
+class ProductCreateView(LoginRequiredMixin,CreateView):
+    model=Product
+    fields = '__all__'
+
+class ProductUpdateView(LoginRequiredMixin,UpdateView):
+    model=Product
+    fields='__all__'
+    template_name_suffix = '_update_form'
+
+
+    #新規作成・編集完了時のリダイレクト先
+    def get_absolute_url(self):
+        return reverse('list')
+
+class ProductDeleteView(LoginRequiredMixin,DeleteView):
+    model=Product
+    success_url=reverse_lazy('list')
+
+
+class ProductDetailView(DetailView):
+    model=Product
+    template_name = 'crud/product_detail.html'
+    context_object_name = 'product'
+
+
+class LoginView(LoginView):
+    form_class=AuthenticationForm
+    template_name='login.html'
+
+
+class LogoutView(LoginRequiredMixin,LogoutView):
+    template_name='top.html'
